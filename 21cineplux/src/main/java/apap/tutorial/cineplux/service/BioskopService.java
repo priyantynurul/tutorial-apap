ff9fd57adf37c351d6328efe04e6ba7d5ac2e3af
Priyanty Nurul Fatimah <priyanty.nurul@ui.ac.id>
2021-09-15 23:52:38 +0700

    push tutorial 2

---
+++ 21cineplux/src/main/java/apap/tutorial/cineplux/service/BioskopService.java
@@ -0,0 +1,15 @@
+package apap.tutorial.cineplux.service;
+
+import apap.tutorial.cineplux.model.BioskopModel;
+import java.util.List;
+
+public interface BioskopService {
+    //Method untuk menambah Bioskop
+    void addBioskop(BioskopModel bioskop);
+
+    //Method untuk mendapatkan daftar Bioskop yang telah tersimpan
+    List<BioskopModel> getBioskopList();
+
+    //Method untuk mendapatkan data sebuah bioskop berdasarkan id bioskop
+    BioskopModel getBioskopByIdBioskop(String idBioskop);
+}
